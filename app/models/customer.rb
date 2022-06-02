02c102c1e005412791851a8b93b04aa890bb8d30
Subshin31 <subhada@shepherdshill.in>
2022-06-02 10:56:35 +0530

    validation 1

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,8 +1,11 @@
 class Customer < ApplicationRecord
 	has_many :orders
 	paginates_per 4
-	validates :name, presence: true
-	validates :name, length: { minimum: 20 }
-	
+	validates :name, presence: true, length: { minimum: 3 }
+	validates :address, presence: true
+	validates :contact_number, length: { minimum: 10 , maximum: 10}
+	validates :email_id, confirmation: true, presence: true
+
+
 end
 
