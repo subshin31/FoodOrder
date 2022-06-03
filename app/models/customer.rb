9ad911aac1983512e9e86f1c8603e806064deced
Subshin31 <subhada@shepherdshill.in>
2022-06-03 17:35:36 +0530

    validation0306

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,8 +3,8 @@ class Customer < ApplicationRecord
 	paginates_per 4
 	validates :name, presence: true, length: { minimum: 3 }
 	validates :address, presence: true, length: { minimum: 10 , maximum: 100}
-	validates :contact_number, length: { minimum: 10 , maximum: 10}, numericality: { only_integer: true},format: { with: /\A\+?[1-9]\d{1,14}\z/i, on: :create}
-	validates :email_id, presence: true, format: { with: /\A([A-Za-z+_.-]+)@(([A-Za-z0-9.-]+\.)+[com]{2,})\z/i, on: :create}, uniqueness: true, on: :update
+	validates :contact_number, length: { minimum: 10 , maximum: 10},format: { with: /\A(^((99|98|97|88|89|87|79|78|77)[1-9]))\d{7}\z/}
+	validates :email_id, presence: true, format: { with: /\A([A-Za-z+_.-]+)@(([A-Za-z0-9.-]+\.)+[com]{2,})\z/i, on: :create}, uniqueness: true, on: :update  
 
 end
 
